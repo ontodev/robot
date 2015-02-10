d6bfc8e8f5da679f36ed2411d167586f9ff86f4e
James A. Overton <james@overton.ca>
2015-02-10 18:25:32 -0500

    Pull CatalogElementHandler out of CatalogXmlIRIMapper
    - add new CatalogElementHandler class
    - simplify CatalogXmlIRIMapper
    - add loggers
    - add Javadoc
    - make Checkstyle happy

--- owltools2-core/src/main/java/owltools2/CatalogXmlIRIMapper.java
+++ owltools2-core/src/main/java/owltools2/CatalogXmlIRIMapper.java
@@ -13,170 +13,169 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-//import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntologyIRIMapper;
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * {@link OWLOntologyIRIMapper} using the mappings from a catalog.xml file.
  */
 public class CatalogXmlIRIMapper implements OWLOntologyIRIMapper {
+    /**
+     * Logger.
+     */
+    private static final Logger logger =
+        LoggerFactory.getLogger(CatalogXmlIRIMapper.class);
 
-	//private static final Logger logger = Logger.getLogger(CatalogXmlIRIMapper.class);
-	
-	private final Map<IRI, IRI> mappings;
-	
-	CatalogXmlIRIMapper(Map<IRI, IRI> mappings) {
-		this.mappings = mappings;
-	}
-	
-	/**
-	 * Create an CatalogXmlIRIMapper from the given catalog.xml file.
-	 * Assume, that relative paths are relative to the catalog file location.
-	 * 
-	 * @param catalogFile
-	 * @throws IOException
-	 */
-	public CatalogXmlIRIMapper(String catalogFile) throws IOException {
-		this(new File(catalogFile).getAbsoluteFile());
-	}
-	
-	/**
-	 * Create an CatalogXmlIRIMapper from the given catalog.xml file.
-	 * Assume, that relative paths are relative to the catalog file location.
-	 * 
-	 * @param catalogFile
-	 * @throws IOException
-	 */
-	public CatalogXmlIRIMapper(File catalogFile) throws IOException {
-		this(catalogFile, catalogFile.getAbsoluteFile().getParentFile());
-	}
-	
-	/**
-	 * Create an CatalogXmlIRIMapper from the given catalog.xml file. 
-	 * Use the parentFolder to resolve relative paths from the catalog file. 
-	 * 
-	 * @param catalogFile
-	 * @param parentFolder
-	 * @throws IOException
-	 */
-	public CatalogXmlIRIMapper(File catalogFile, File parentFolder) throws IOException {
-		this(parseCatalogXml(new FileInputStream(catalogFile), parentFolder));
-	}
-	
-	/**
-	 * Create an CatalogXmlIRIMapper from the given catalog URL.
-	 * Assume, there are no relative paths in the catalog file.
-	 * 
-	 * @param catalogURL
-	 * @throws IOException
-	 */
-	public CatalogXmlIRIMapper(URL catalogURL) throws IOException {
-		if ("file".equals(catalogURL.getProtocol())) {
-			try {
-				File catalogFile = new File(catalogURL.toURI());
-				mappings = parseCatalogXml(new FileInputStream(catalogFile), catalogFile.getParentFile());
-			} catch (URISyntaxException e) {
-				throw new IOException(e);
-			}
-		}
-		else {
-			mappings = parseCatalogXml(catalogURL.openStream(), null);
-		}
-	}
-	
-	/**
-	 * Create an CatalogXmlIRIMapper from the given catalog URL.
-	 * Use the parentFolder to resolve relative paths from the catalog file. 
-	 * 
-	 * @param catalogURL
-	 * @param parentFolder
-	 * @throws IOException
-	 */
-	public CatalogXmlIRIMapper(URL catalogURL, File parentFolder) throws IOException {
-		this(parseCatalogXml(catalogURL.openStream(), parentFolder));
-	}
-	
-	@Override
-	public IRI getDocumentIRI(IRI ontologyIRI) {
-		return mappings.get(ontologyIRI);
-	}
-	
-	/**
-	 * Parse the inputStream as a catalog.xml file and extract IRI mappings.
-	 * 
-	 * Optional: Resolve relative file paths with the given parent folder.
-	 * 
-	 * @param inputStream input stream (never null)
-	 * @param parentFolder folder or null
-	 * @return mappings
-	 * @throws IOException
-	 * @throws IllegalArgumentException if input stream is null
-	 */
-	static Map<IRI, IRI> parseCatalogXml(InputStream inputStream, final File parentFolder) throws IOException {
-		if (inputStream == null) {
-			throw new IllegalArgumentException("InputStream should never be null, missing resource?");
-		}
-		
-		// use the Java built-in SAX parser 
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-	    factory.setValidating(false);
-	    
-	    try {
-	    	final Map<IRI, IRI> mappings = new HashMap<IRI, IRI>();
-			SAXParser saxParser = factory.newSAXParser();
-			saxParser.parse(inputStream, new DefaultHandler(){
+    /**
+     * Mappings from OntologyIRIs to (usually) file IRIs,
+     * allowing a layer of indirection when importing ontologies.
+     */
+    private final Map<IRI, IRI> mappings;
 
-				@Override
-				public void startElement(String uri, String localName,
-						String qName, Attributes attributes)
-						throws SAXException 
-				{
-					// only look at 'uri' tags
-					// does not check any parent tags
-					if ("uri".equals(qName)) {
-						IRI original = null;
-						IRI mapped = null;
-						String nameString = attributes.getValue("name");
-						if (nameString != null) {
-							original = IRI.create(nameString);
-						}
-						String mappedString = attributes.getValue("uri");
-						if (mappedString != null) {
-							if (parentFolder != null && mappedString.indexOf(":") < 0) {
-								// there is a parent folder and the mapping is not an IRI or URL
-								File file = new File(mappedString);
-								if (!file.isAbsolute()) {
-									file = new File(parentFolder, mappedString);
-								}
-								try {
-									file = file.getCanonicalFile();
-									mapped = IRI.create(file);
-								} catch (IOException e) {
-									//logger.warn("Skipping mapping: "+nameString+"   "+mappedString, e);
-								}
-							}
-							else {
-								mapped = IRI.create(mappedString);
-							}
-						}
-						
-						if (original != null && mapped != null) {
-							mappings.put(original, mapped);
-						}
-					}
-				}
-			});
-			return mappings;
-		} catch (ParserConfigurationException e) {
-			throw new IOException(e);
-		} catch (SAXException e) {
-			throw new IOException(e);
-		} finally {
-			inputStream.close();
-		}
-	}
+    /**
+     * Initialize with IRI mappings.
+     *
+     * @param mappings initial mappings from IRIs to IRIs
+     */
+    CatalogXmlIRIMapper(Map<IRI, IRI> mappings) {
+        this.mappings = mappings;
+    }
+
+    /**
+     * Given a String path to a catalog file, create a CatalogXmlIRIMapper
+     * that interprets any relative paths as relative
+     * to the catalog file location.
+     *
+     * @param catalogFile the String path to the catalog file
+     * @throws IOException on any problem
+     */
+    public CatalogXmlIRIMapper(String catalogFile) throws IOException {
+        this(new File(catalogFile).getAbsoluteFile());
+    }
+
+    /**
+     * Given a catalog File, create a CatalogXmlIRIMapper
+     * that interprets any relative paths as relative
+     * to the catalog file location.
+     *
+     * @param catalogFile the catalog File
+     * @throws IOException on any problem
+     */
+    public CatalogXmlIRIMapper(File catalogFile) throws IOException {
+        this(catalogFile, catalogFile.getAbsoluteFile().getParentFile());
+    }
+
+    /**
+     * Given a catalog File and a parent folder, create a CatalogXmlIRIMapper
+     * that interprets any relative paths as relative to the parent folder.
+     *
+     * @param catalogFile the catalog File
+     * @param parentFolder the File for the parent folder
+     * @throws IOException on any problem
+     */
+    public CatalogXmlIRIMapper(File catalogFile, File parentFolder)
+            throws IOException {
+        this(parseCatalogXml(new FileInputStream(catalogFile), parentFolder));
+    }
+
+    /**
+     * Given an IRI, create a CatalogXmlIRIMapper
+     * that interprets any relative paths as relative
+     * to the catalog file location.
+     *
+     * @param catalogIRI the IRI of the catalog file
+     * @throws IOException on any problem
+     */
+    public CatalogXmlIRIMapper(IRI catalogIRI) throws IOException {
+        this(catalogIRI.toURI().toURL());
+    }
+
+    /**
+     * Given an URL, create a CatalogXmlIRIMapper
+     * that interprets any relative paths as relative
+     * to the catalog file location.
+     *
+     * @param catalogURL the URL of the catalog file
+     * @throws IOException on any problem
+     */
+    public CatalogXmlIRIMapper(URL catalogURL) throws IOException {
+        if ("file".equals(catalogURL.getProtocol())) {
+            try {
+                File catalogFile = new File(catalogURL.toURI());
+                mappings = parseCatalogXml(
+                        new FileInputStream(catalogFile),
+                        catalogFile.getParentFile());
+            } catch (URISyntaxException e) {
+                throw new IOException(e);
+            }
+        } else {
+            mappings = parseCatalogXml(catalogURL.openStream(), null);
+        }
+    }
+
+    /**
+     * Given an URL and a parent folder, create a CatalogXmlIRIMapper
+     * that interprets any relative paths as relative to the parent folder.
+     *
+     * @param catalogURL the URL of the catalog file
+     * @param parentFolder the File for the parent folder
+     * @throws IOException on any problem
+     */
+    public CatalogXmlIRIMapper(URL catalogURL, File parentFolder)
+            throws IOException {
+        this(parseCatalogXml(catalogURL.openStream(), parentFolder));
+    }
+
+    /**
+     * Given an IRI, return the mapped IRI.
+     *
+     * @param ontologyIRI the IRI that we want to look up
+     * @return the mapped IRI, usually to a local file
+     */
+    @Override
+    public IRI getDocumentIRI(IRI ontologyIRI) {
+        return mappings.get(ontologyIRI);
+    }
+
+    /**
+     * Parse the inputStream as a catalog.xml file and extract IRI mappings.
+     *
+     * Optional: Resolve relative file paths with the given parent folder.
+     *
+     * @param inputStream input stream (never null)
+     * @param parentFolder folder or null
+     * @return mappings
+     * @throws IOException on general IO problems
+     * @throws IllegalArgumentException if input stream is null
+     */
+    static Map<IRI, IRI> parseCatalogXml(InputStream inputStream,
+            final File parentFolder)
+            throws IOException, IllegalArgumentException {
+        if (inputStream == null) {
+            throw new IllegalArgumentException(
+                    "InputStream should never be null, missing resource?");
+        }
+
+        // use the Java built-in SAX parser
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setValidating(false);
+
+        try {
+            final Map<IRI, IRI> mappings = new HashMap<IRI, IRI>();
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse(inputStream,
+                    new CatalogElementHandler(parentFolder, mappings));
+            return mappings;
+        } catch (ParserConfigurationException e) {
+            throw new IOException(e);
+        } catch (SAXException e) {
+            throw new IOException(e);
+        } finally {
+            inputStream.close();
+        }
+    }
 }
