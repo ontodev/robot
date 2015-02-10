ae5889085c17b05d141497374cfecaf2bf627dd6
James A. Overton <james@overton.ca>
2015-02-10 18:28:21 -0500

    Clean up core files
    - refactor/remove some IOHelper methods
    - add loggers
    - add Javadoc
    - make Checkstyle happy

--- owltools2-core/src/main/java/owltools2/ExtractOperation.java
+++ owltools2-core/src/main/java/owltools2/ExtractOperation.java
@@ -1,44 +1,66 @@
 package owltools2;
 
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLEntity;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-
-import uk.ac.manchester.cs.owlapi.modularity.SyntacticLocalityModuleExtractor;
 import uk.ac.manchester.cs.owlapi.modularity.ModuleType;
+import uk.ac.manchester.cs.owlapi.modularity.SyntacticLocalityModuleExtractor;
 
 /**
- * Implements the extract operation.
+ * Extract a set of OWLEntities from the input ontology to an output ontology.
+ * Uses the OWLAPI's SyntacticLocalityModuleExtractor (SLME).
  *
  * @author <a href="mailto:james@overton.ca">James A. Overton</a>
  */
 public class ExtractOperation {
+    /**
+     * Logger.
+     */
+    private static final Logger logger =
+        LoggerFactory.getLogger(ExtractOperation.class);
 
-  /**
-   * Extract a set of terms from an ontology.
-   * TODO: implement other methods for cleaning results.
-   *
-   * @return a new ontology (with a new manager)
-   */
-  public static OWLOntology extract(OWLOntology inputOntology,
-      Set<IRI> terms, IRI outputIRI)
-      throws OWLOntologyCreationException {
-    System.out.println("Extracting");
-    Set<OWLEntity> entities = new HashSet<OWLEntity>();
-    for(IRI term: terms) {
-      entities.addAll(inputOntology.getEntitiesInSignature(term, true));
+    /**
+     * Extract a set of terms from an ontology
+     * using the OWLAPI's SyntacticLocalityModuleExtractor (SLME).
+     *
+     * @param inputOntology the ontology to extract from
+     * @param terms a set of IRIs for terms to extract
+     * @param outputIRI the OntologyIRI of the new ontology
+     * @param moduleType determines the type of extraction; defaults to STAR
+     * @return a new ontology (with a new manager)
+     * @throws OWLOntologyCreationException on any OWLAPI problem
+     */
+    public static OWLOntology extract(OWLOntology inputOntology,
+            Set<IRI> terms, IRI outputIRI, ModuleType moduleType)
+            throws OWLOntologyCreationException {
+        logger.debug("Extracting...");
+
+        Set<OWLEntity> entities = new HashSet<OWLEntity>();
+        for (IRI term: terms) {
+            entities.addAll(inputOntology.getEntitiesInSignature(term, true));
+        }
+
+        ModuleType type = moduleType;
+        if (type == null) {
+            type = ModuleType.STAR;
+        }
+
+        SyntacticLocalityModuleExtractor extractor =
+            new SyntacticLocalityModuleExtractor(
+                    inputOntology.getOWLOntologyManager(),
+                    inputOntology,
+                    type);
+
+        return OWLManager.createOWLOntologyManager().createOntology(
+                extractor.extract(entities),
+                outputIRI);
     }
-    SyntacticLocalityModuleExtractor extractor =
-      new SyntacticLocalityModuleExtractor(
-        inputOntology.getOWLOntologyManager(), inputOntology, ModuleType.STAR);
-    return OWLManager.createOWLOntologyManager().createOntology(
-        extractor.extract(entities),
-        outputIRI);
-  }
 }
--- owltools2-core/src/main/java/owltools2/IOHelper.java
+++ owltools2-core/src/main/java/owltools2/IOHelper.java
@@ -1,22 +1,22 @@
 package owltools2;
 
-import java.util.Set;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
-
-import owltools2.CatalogXmlIRIMapper;
 
 /**
  * Provides convenience methods for working with ontology and term files.
@@ -24,137 +24,247 @@ import owltools2.CatalogXmlIRIMapper;
  * @author <a href="mailto:james@overton.ca">James A. Overton</a>
  */
 public class IOHelper {
+    /**
+     * Logger.
+     */
+    private static final Logger logger =
+        LoggerFactory.getLogger(IOHelper.class);
 
-  // TODO: add methods for manipulating a shared PrefixManager
-  
-  /**
-   * Try to guess the location of the catalog.xml file.
-   */
-  public File guessCatalogFile(File ontologyFile) {
-    String path = ontologyFile.getParent();
-    String catalogPath = path + "/catalog-v001.xml";
-    return new File(catalogPath);
-  }
-
-  public OWLOntology loadOntology(String ontologyPath)
-      throws IOException {
-    File ontologyFile = new File(ontologyPath);
-    File catalogFile = guessCatalogFile(ontologyFile); 
-    return loadOntology(ontologyFile, catalogFile);
-  }
-
-  public OWLOntology loadOntology(String ontologyPath, boolean useCatalog)
-      throws IOException {
-    File ontologyFile = new File(ontologyPath);
-    File catalogFile = null;
-    if(useCatalog) {
-      catalogFile = guessCatalogFile(ontologyFile); 
+    /**
+     * Try to guess the location of the catalog.xml file.
+     * Looks in the directory of the given ontology file for a catalog file.
+     *
+     * @param ontologyFile the
+     * @return the guessed catalog File; may not exist!
+     */
+    public File guessCatalogFile(File ontologyFile) {
+        String path = ontologyFile.getParent();
+        String catalogPath = path + "/catalog-v001.xml";
+        return new File(catalogPath);
     }
-    return loadOntology(ontologyFile, catalogFile);
-  }
 
-  public OWLOntology loadOntology(String ontologyPath, String catalogPath)
-      throws IOException {
-    File ontologyFile = new File(ontologyPath);
-    File catalogFile =  new File(catalogPath);
-    return loadOntology(ontologyFile, catalogFile);
-  }
-
-  public OWLOntology loadOntology(File ontologyFile)
-      throws IOException {
-    File catalogFile = guessCatalogFile(ontologyFile); 
-    return loadOntology(ontologyFile, catalogFile);
-  }
-
-  public OWLOntology loadOntology(File ontologyFile, boolean useCatalog)
-      throws IOException {
-    File catalogFile = null;
-    if(useCatalog) {
-      catalogFile = guessCatalogFile(ontologyFile); 
+    /**
+     * Load an ontology from a String path, using a catalog file if available.
+     *
+     * @param ontologyPath the path to the ontology file
+     * @return a new ontology object, with a new OWLManager
+     * @throws IOException on any problem
+     */
+    public OWLOntology loadOntology(String ontologyPath)
+            throws IOException {
+        File ontologyFile = new File(ontologyPath);
+        File catalogFile = guessCatalogFile(ontologyFile);
+        return loadOntology(ontologyFile, catalogFile);
     }
-    return loadOntology(ontologyFile, catalogFile);
-  }
 
-  public OWLOntology loadOntology(File ontologyFile, File catalogFile)
-      throws IOException {
-    OWLOntology ontology = null;
-    try {
-      OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-      if(catalogFile != null && catalogFile.isFile()) {
-        manager.addIRIMapper(new CatalogXmlIRIMapper(catalogFile));
-      }
-      ontology = manager.loadOntologyFromOntologyDocument(ontologyFile);
-    } catch (OWLOntologyCreationException e) {
-      throw new IOException(e);
+    /**
+     * Load an ontology from a String path, with option to use catalog file.
+     *
+     * @param ontologyPath the path to the ontology file
+     * @param useCatalog when true, a catalog file will be used if one is found
+     * @return a new ontology object, with a new OWLManager
+     * @throws IOException on any problem
+     */
+    public OWLOntology loadOntology(String ontologyPath, boolean useCatalog)
+            throws IOException {
+        File ontologyFile = new File(ontologyPath);
+        File catalogFile = null;
+        if (useCatalog) {
+            catalogFile = guessCatalogFile(ontologyFile);
+        }
+        return loadOntology(ontologyFile, catalogFile);
     }
-    return ontology;
-  }
 
-  //public OWLOntology loadOntology(IRI ontologyIRI) {
-  //  OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-  //  return manager.loadOntologyInputOntologyDocument(ontologyIRI);
-  //}
-  //public OWLOntology loadOntology(IRI ontologyIRI, Bool use-catalog)
-  //public OWLOntology loadOntology(IRI ontologyIRI, IRI catalogIRI)
-
-  //public OWLOntology saveOntology(OWLOntology ontology, String ontologyPath)
-
-  public void saveOntology(OWLOntology ontology, File ontologyFile)
-      throws IOException {
-    try {
-    ontology.getOWLOntologyManager().saveOntology(
-        ontology, IRI.create(ontologyFile));
-    } catch (OWLOntologyStorageException e) {
-      throw new IOException(e);
+    /**
+     * Load an ontology from a String path, with optional catalog file.
+     *
+     * @param ontologyPath the path to the ontology file
+     * @param catalogPath the path to the catalog file
+     * @return a new ontology object, with a new OWLManager
+     * @throws IOException on any problem
+     */
+    public OWLOntology loadOntology(String ontologyPath, String catalogPath)
+            throws IOException {
+        File ontologyFile = new File(ontologyPath);
+        File catalogFile  = new File(catalogPath);
+        return loadOntology(ontologyFile, catalogFile);
     }
-  }
-  //public OWLOntology saveOntology(OWLOntology ontology, IRI ontologyIRI)
 
-  
-  /**
-   * Remove comments, trim lines, remove empty lines.
-   * A comment is a space or newline followed by a '#', to the end of the line.
-   * This excludes '#' characters in IRIs.
-   */
-  public Set<String> extractTerms(String input) {
-    Set<String> results = new HashSet<String>();
-    List<String> lines = Arrays.asList(
-        input.replaceAll("\\r", "").split("\\n"));
-    for(String line: lines) {
-      String result = line.replaceFirst("($|\\s)#.*$", "").trim();
-      if(!result.isEmpty()) {
-        results.add(result);
-      }
+    /**
+     * Load an ontology from a File, using a catalog file if available.
+     *
+     * @param ontologyFile the ontology file to load
+     * @return a new ontology object, with a new OWLManager
+     * @throws IOException on any problem
+     */
+    public OWLOntology loadOntology(File ontologyFile)
+            throws IOException {
+        File catalogFile = guessCatalogFile(ontologyFile);
+        return loadOntology(ontologyFile, catalogFile);
     }
-    return results;
-  }
 
-  public Set<IRI> createIRIs(Set<String> terms) throws IllegalArgumentException {
-    Set<IRI> iris = new HashSet<IRI>();
-    for(String term: terms) {
-      // TODO: use prefix manager to expand IRIs
-      iris.add(IRI.create(term));
+    /**
+     * Load an ontology from a File, with option to use a catalog file.
+     *
+     * @param ontologyFile the ontology file to load
+     * @param useCatalog when true, a catalog file will be used if one is found
+     * @return a new ontology object, with a new OWLManager
+     * @throws IOException on any problem
+     */
+    public OWLOntology loadOntology(File ontologyFile, boolean useCatalog)
+            throws IOException {
+        File catalogFile = null;
+        if (useCatalog) {
+            catalogFile = guessCatalogFile(ontologyFile);
+        }
+        return loadOntology(ontologyFile, catalogFile);
     }
-    return iris;
-  }
 
-  /**
-   * Parse a set of IRIs from a space-separated string, ignoring '#' comments.
-   */
-  public Set<IRI> parseTerms(String input) throws IllegalArgumentException {
-    return createIRIs(extractTerms(input));
-  }
+    /**
+     * Load an ontology from a File, with optional catalog File.
+     *
+     * @param ontologyFile the ontology file to load
+     * @param catalogFile the catalog file to use
+     * @return a new ontology object, with a new OWLManager
+     * @throws IOException on any problem
+     */
+    public OWLOntology loadOntology(File ontologyFile, File catalogFile)
+            throws IOException {
+        logger.debug("Loading ontology {} with catalog file {}",
+                ontologyFile, catalogFile);
 
-  public Set<IRI> loadTerms(String path) throws IOException {
-    return loadTerms(new File(path));
-  }
+        OWLOntology ontology = null;
+        try {
+            OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+            if (catalogFile != null && catalogFile.isFile()) {
+                manager.addIRIMapper(new CatalogXmlIRIMapper(catalogFile));
+            }
+            ontology = manager.loadOntologyFromOntologyDocument(ontologyFile);
+        } catch (OWLOntologyCreationException e) {
+            throw new IOException(e);
+        }
+        return ontology;
+    }
 
-  /**
-   * Load a set of IRIs from a file (space-separated, ignoring '#' comments).
-   */
-  public Set<IRI> loadTerms(File file) throws IOException {
-    String content = new Scanner(file).useDelimiter("\\Z").next();
-    return parseTerms(content);
-  }
+    /**
+     * Save an ontology to a String path.
+     *
+     * @param ontology the ontology to save
+     * @param ontologyPath the path to save the ontology to
+     * @return the saved ontology
+     * @throws IOException on any problem
+     */
+    public OWLOntology saveOntology(OWLOntology ontology, String ontologyPath)
+            throws IOException {
+        return saveOntology(ontology, new File(ontologyPath));
+    }
+
+    /**
+     * Save an ontology to a File.
+     *
+     * @param ontology the ontology to save
+     * @param ontologyFile the file to save the ontology to
+     * @return the saved ontology
+     * @throws IOException on any problem
+     */
+    public OWLOntology saveOntology(OWLOntology ontology, File ontologyFile)
+            throws IOException {
+        return saveOntology(ontology, IRI.create(ontologyFile));
+    }
+
+    /**
+     * Save an ontology to an IRI.
+     *
+     * @param ontology the ontology to save
+     * @param ontologyIRI the IRI to save the ontology to
+     * @return the saved ontology
+     * @throws IOException on any problem
+     */
+    public OWLOntology saveOntology(final OWLOntology ontology, IRI ontologyIRI)
+            throws IOException {
+        logger.debug("Saving ontology {} with to IRI {}",
+                ontology, ontologyIRI);
+
+        try {
+            ontology.getOWLOntologyManager().saveOntology(
+                    ontology, ontologyIRI);
+        } catch (OWLOntologyStorageException e) {
+            throw new IOException(e);
+        }
+        return ontology;
+    }
+
+
+    /**
+     * Extract a set of term identifiers from an input string
+     * by removing comments, trimming lines, and removing empty lines.
+     * A comment is a space or newline followed by a '#',
+     * to the end of the line. This excludes '#' characters in IRIs.
+     *
+     * @param input the String containing the term identifiers
+     * @return a set of term identifier strings
+     */
+    public Set<String> extractTerms(String input) {
+        Set<String> results = new HashSet<String>();
+        List<String> lines = Arrays.asList(
+                input.replaceAll("\\r", "").split("\\n"));
+        for (String line: lines) {
+            String result = line.replaceFirst("($|\\s)#.*$", "").trim();
+            if (!result.isEmpty()) {
+                results.add(result);
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Given a set of term identifier strings, return a set of IRIs.
+     *
+     * @param terms the set of term identifier strings
+     * @return the set of IRIs
+     * @throws IllegalArgumentException if term identifier is not a valid IRI
+     */
+    public Set<IRI> createIRIs(Set<String> terms)
+            throws IllegalArgumentException {
+        Set<IRI> iris = new HashSet<IRI>();
+        for (String term: terms) {
+            iris.add(IRI.create(term));
+        }
+        return iris;
+    }
+
+    /**
+     * Parse a set of IRIs from a space-separated string, ignoring '#' comments.
+     *
+     * @param input the string containing the IRI strings
+     * @return the set of IRIs
+     * @throws IllegalArgumentException if term identifier is not a valid IRI
+     */
+    public Set<IRI> parseTerms(String input) throws IllegalArgumentException {
+        return createIRIs(extractTerms(input));
+    }
+
+    /**
+     * Load a set of IRIs from a file.
+     *
+     * @param path the path to the file containing the terms
+     * @return the set of IRIs
+     * @throws IOException on any problem
+     */
+    public Set<IRI> loadTerms(String path) throws IOException {
+        return loadTerms(new File(path));
+    }
+
+    /**
+     * Load a set of IRIs from a file.
+     *
+     * @param file the File containing the terms
+     * @return the set of IRIs
+     * @throws IOException on any problem
+     */
+    public Set<IRI> loadTerms(File file) throws IOException {
+        String content = new Scanner(file).useDelimiter("\\Z").next();
+        return parseTerms(content);
+    }
 
 }
