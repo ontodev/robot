c08526b6280ede9595024c76f362f1c6e57040d8
James A. Overton <james@overton.ca>
2015-02-17 12:26:02 -0500

    Add basic tests for CommandManager

---
+++ owltools2-command/src/test/java/owltools2/CommandManagerTest.java
@@ -0,0 +1,109 @@
+package owltools2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import org.apache.commons.cli.Options;
+
+/**
+ * Tests for {@link CommandManager}.
+ */
+public class CommandManagerTest {
+    /**
+     * Shared manager.
+     */
+    private CommandManager manager = new CommandManager();
+
+    /**
+     * Shared options.
+     */
+    private Options options = new Options();
+
+    /**
+     * Create a new manager with new options and new commands.
+     */
+    private void reset() {
+        options = new Options();
+        options.addOption("h", "help",  false, "help option, without value");
+        options.addOption("i", "input", true,  "input option, with value");
+
+        manager = new CommandManager();
+        manager.addCommand("mock1", new MockCommand());
+        manager.addCommand("mock2", new MockCommand());
+        manager.addCommand("mock3", new MockCommand());
+    }
+
+    /**
+     * Convenience method for splitting an argument string;
+     * just splits at whitespace, does not handle quoted strings, etc.
+     *
+     * @param arg the argument string
+     * @return an array of arguments
+     */
+    private String[] splitArgs(String arg) {
+        return arg.split("\\s+");
+    }
+
+    /**
+     * Convenience method for asserting and counting parsed options.
+     *
+     * @param message a message to pass on test failure
+     * @param count an integer with the expected count
+     * @param arg a single string of command-line arguments; will be split
+     * @throws Exception on any problem
+     */
+    private void countOptions(String message, int count, String arg)
+            throws Exception {
+        List<String> args =
+            new ArrayList<String>(Arrays.asList(splitArgs(arg)));
+        List<String> used = manager.getOptionArgs(options, args);
+        assertEquals(message, count, used.size());
+    }
+
+    /**
+     * Test the options are being parsed correctly.
+     *
+     * @throws Exception on any problem
+     */
+    @Test
+    public void testCountOptions() throws Exception {
+        reset();
+        countOptions("No options", 0, "");
+        countOptions("No options", 0, "foo bar");
+
+        countOptions("Short help option", 1, "-h");
+        countOptions("Short help option", 1, "-h foo bar");
+        countOptions("Long help option",  1, "--help");
+        countOptions("Long help option",  1, "--help foo bar");
+
+        countOptions("Short input option", 2, "-i foo");
+        countOptions("Short input option", 2, "-i foo bar");
+        countOptions("Long input option",  2, "--input foo");
+        countOptions("Long input option",  2, "--input foo bar");
+
+        countOptions("Multiple options", 3, "-h --input foo bar");
+
+        countOptions("Unknown option", 0, "-f bar");
+        countOptions("Unknown option", 1, "-h --foo bar");
+        countOptions("Unknown option", 2, "-i baz --foo bar");
+        countOptions("Unknown option", 1, "-h --foo bar");
+        countOptions("Unknown option", 3, "-h -i baz --foo bar");
+    }
+
+    /**
+     * Test that commands are being executed.
+     *
+     * @throws Exception on any problem
+     */
+    @Test
+    public void testExecute() throws Exception {
+        reset();
+        manager.execute(null, splitArgs("--help mock1"));
+        manager.execute(null, splitArgs("--help mock1 mock2"));
+        //manager.execute(null, splitArgs("--help mock1 --local foo"));
+    }
+}
