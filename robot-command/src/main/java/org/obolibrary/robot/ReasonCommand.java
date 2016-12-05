a5a068e78988693ba4fbe20a125765ae7fbcb4c7
Chris Mungall <cjm@berkeleybop.org>
2016-12-04 16:48:18 -0800

    Optionally fail if NC equivalency detected. Fixes #123

--- robot-command/src/main/java/org/obolibrary/robot/ReasonCommand.java
+++ robot-command/src/main/java/org/obolibrary/robot/ReasonCommand.java
@@ -44,6 +44,8 @@ public class ReasonCommand implements Command {
                 "annotate all inferred axioms (only when -n is passed)");
         o.addOption("x", "exclude-duplicate-axioms",     true,
                 "do not add an axiom if it exists in import chain");
+        o.addOption("e", "equivalent-classes-allowed",     true,
+                "if false then detecting equivalent classes exits with error");
         o.addOption("i", "input",     true, "reason ontology from a file");
         o.addOption("I", "input-iri", true, "reason ontology from an IRI");
         o.addOption("o", "output",    true, "save reasoned ontology to a file");
