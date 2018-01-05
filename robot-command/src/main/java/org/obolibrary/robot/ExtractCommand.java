6fe01c84811aa3d7c93af1184149cd5cd400c8cc
Rebecca Tauber <becky.tauber@ninepts.com>
2018-01-05 09:15:29 -0500

    First pass at #212 (allow --source-file csv)

--- robot-command/src/main/java/org/obolibrary/robot/ExtractCommand.java
+++ robot-command/src/main/java/org/obolibrary/robot/ExtractCommand.java
@@ -1,7 +1,7 @@
 package org.obolibrary.robot;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -9,8 +9,10 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
+import org.apache.commons.io.FileUtils;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
+
 import uk.ac.manchester.cs.owlapi.modularity.ModuleType;
 
 /**
@@ -42,6 +44,8 @@ public class ExtractCommand implements Command {
         o.addOption("m", "method",    true, "extract method to use");
         o.addOption("t", "term",      true, "term to extract");
         o.addOption("T", "term-file", true, "load terms from a file");
+        o.addOption("s", "source-file", true, 
+        		"load terms from multiple sources");
         o.addOption("u", "upper-term",  true, "upper level term to extract");
         o.addOption("U", "upper-terms", true, "upper level terms to extract");
         o.addOption("l", "lower-term",  true, "lower level term to extract");
@@ -124,66 +128,126 @@ public class ExtractCommand implements Command {
         if (line == null) {
             return null;
         }
-
+        
         IOHelper ioHelper = CommandLineHelper.getIOHelper(line);
-        state = CommandLineHelper.updateInputOntology(ioHelper, state, line);
-        OWLOntology inputOntology = state.getOntology();
+        String sourceFile = CommandLineHelper.getOptionalValue(line, "source-file");
+        
+        // Extract from multiple sources, no --input
+        if (sourceFile != null) {
+        	// TODO: Should we do something with state?
+        	String source = FileUtils.readFileToString(new File(sourceFile));
+        	for (String row : source.split(System.lineSeparator())) {
+        		// Skip the header
+        		if (row.startsWith("ID,IRI")) {
+        			continue;
+        		}
+        		
+        		// Get parameters for the extraction
+        		// ID, IRI, method, term file, output
+        		String[] params = row.split(",");
+        		IRI iri = IRI.create(params[1]);
+        		String method = params[2].toLowerCase();
+        		String termFile = params[3];
+        		String output = params[4];
 
-        IRI outputIRI = CommandLineHelper.getOutputIRI(line);
-        if (outputIRI == null) {
-            outputIRI = inputOntology.getOntologyID().getOntologyIRI().orNull();
-        }
+        		ModuleType moduleType = getModuleType(method);
+        		// Input to extract is retrieved from IRI
+        		OWLOntology inputOntology = ioHelper.loadOntology(iri);
+        		
+        		if (method.equals("mireot")) {
+        			String[] levels = FileUtils
+        					.readFileToString(new File(termFile)).split("-");
 
-        String method = CommandLineHelper
-            .getDefaultValue(line, "method", "mireot")
-            .trim().toLowerCase();
-        ModuleType moduleType = null;
-        if (method.equals("star")) {
-            moduleType = ModuleType.STAR;
-        } else if (method.equals("top")) {
-            moduleType = ModuleType.TOP;
-        } else if (method.equals("bot")) {
-            moduleType = ModuleType.BOT;
-        }
+        			// Get upper boundaries
+        			Set<IRI> upperIRIs = new HashSet<>();
+        			for (String termString :
+        				levels[0].split(System.lineSeparator())) {
+        				upperIRIs.addAll(ioHelper.parseTerms(termString));
+        			}
+        			
+        			// Get lower boundaries
+        			Set<IRI> lowerIRIs = new HashSet<>();
+        			for (String termString :
+        				levels[1].split(System.lineSeparator())) {
+        				lowerIRIs.addAll(ioHelper.parseTerms(termString));
+        			}
+        			
+        			outputOntology = MireotOperation.mireotExtract(ioHelper, 
+        					upperIRIs, lowerIRIs, null, inputOntology);
+        			
+        		} else if (moduleType != null) {
+        			// Get the set of terms
+            		Set<String> termStrings = new HashSet<>();
+            		termStrings.add(FileUtils.readFileToString(new File(termFile)));
+            		Set<IRI> terms = new HashSet<>();
+            		for (String termString: termStrings) {
+                        terms.addAll(ioHelper.parseTerms(termString));
+                    }
+            		
+    				outputOntology = ExtractOperation.extract(inputOntology,
+                            terms, iri, moduleType);
+        		} else {
+        			throw new Exception("Method must be: MIREOT, STAR, TOP, BOT");
+        		}
+        		
+        		ioHelper.saveOntology(outputOntology, output);
+        	}
 
-        if (method.equals("mireot")) {
-            List<OWLOntology> outputOntologies = new ArrayList<OWLOntology>();
-
-            Set<IRI> upperIRIs = CommandLineHelper.getTerms(
-                    ioHelper, line, "upper-term", "upper-terms");
-            if (upperIRIs != null && upperIRIs.size() == 0) {
-                upperIRIs = null;
-            }
-            Set<IRI> lowerIRIs = CommandLineHelper.getTerms(
-                    ioHelper, line, "lower-term", "lower-terms");
-            if (lowerIRIs != null && lowerIRIs.size() != 0) {
-                outputOntologies.add(
-                    MireotOperation.getAncestors(inputOntology,
-                        upperIRIs, lowerIRIs, null));
-            }
-
-            Set<IRI> branchIRIs = CommandLineHelper.getTerms(
-                    ioHelper, line, "branch-from-term", "branch-from-terms");
-            if (branchIRIs != null && branchIRIs.size() != 0) {
-                outputOntologies.add(
-                    MireotOperation.getDescendants(inputOntology,
-                        branchIRIs, null));
-            }
-
-            outputOntology = MergeOperation.merge(outputOntologies);
-        } else if (moduleType != null) {
-            outputOntology = ExtractOperation.extract(
-                    inputOntology,
-                    CommandLineHelper.getTerms(ioHelper, line),
-                    outputIRI,
-                    moduleType);
+        // Extract from one source: --input
         } else {
-            throw new Exception("Method must be: MIREOT, STAR, TOP, BOT");
+	        state = CommandLineHelper.updateInputOntology(ioHelper, state, line);
+	        OWLOntology inputOntology = state.getOntology();
+	
+	        IRI outputIRI = CommandLineHelper.getOutputIRI(line);
+	        if (outputIRI == null) {
+	            outputIRI = inputOntology.getOntologyID().getOntologyIRI().orNull();
+	        }
+	        
+	        String method = CommandLineHelper
+	                .getDefaultValue(line, "method", "mireot")
+	                .trim().toLowerCase();
+	        ModuleType moduleType = getModuleType(method);
+
+	        if (method.equals("mireot")) {
+	    		Set<IRI> upperIRIs = CommandLineHelper.getTerms(
+	                    ioHelper, line, "upper-term", "upper-terms");
+	    		Set<IRI> lowerIRIs = CommandLineHelper.getTerms(
+	                    ioHelper, line, "lower-term", "lower-terms");
+	    		Set<IRI> branchIRIs = CommandLineHelper.getTerms(
+	                    ioHelper, line, "branch-from-term", "branch-from-terms");
+	            outputOntology = MireotOperation.mireotExtract(ioHelper, upperIRIs,
+	            		lowerIRIs, branchIRIs, inputOntology);
+	
+	        } else if (moduleType != null) {
+	        	Set<IRI> terms = CommandLineHelper.getTerms(ioHelper, line);
+	            outputOntology = ExtractOperation.extract(inputOntology, terms,
+	                    outputIRI, moduleType);
+	        } else {
+	            throw new Exception("Method must be: MIREOT, STAR, TOP, BOT");
+	        }
+	
+	        CommandLineHelper.maybeSaveOutput(line, outputOntology);
+	        state.setOntology(outputOntology);
         }
 
-        CommandLineHelper.maybeSaveOutput(line, outputOntology);
-
-        state.setOntology(outputOntology);
         return state;
+    }
+    
+    /**
+     * Get the ModuleType based on provided method string.
+     * 
+     * @param method String corresponding to the ModuleType
+     * @return ModuleType, or null if not STAR, TOP, or BOT
+     */
+    private static ModuleType getModuleType(String method) {
+    	if (method.equals("star")) {
+            return ModuleType.STAR;
+        } else if (method.equals("top")) {
+        	return ModuleType.TOP;
+        } else if (method.equals("bot")) {
+        	return ModuleType.BOT;
+        } else {
+        	return null;
+        }
     }
 }
