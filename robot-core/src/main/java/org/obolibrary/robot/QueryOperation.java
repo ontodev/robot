51cf39c447c94fd5f30d90ae05eb5c034fabcfb6
James A. Overton <james@overton.ca>
2018-01-17 11:39:21 -0500

    Format tweak for QueryOperation

--- robot-core/src/main/java/org/obolibrary/robot/QueryOperation.java
+++ robot-core/src/main/java/org/obolibrary/robot/QueryOperation.java
@@ -297,7 +297,7 @@ public class QueryOperation {
    * @param output the output stream to write to
    */
   public static void writeResult(ResultSet resultSet, Lang format, OutputStream output) {
-    if(format == null) {
+    if (format == null) {
       format = Lang.CSV;
     }
     ResultSetMgr.write(output, resultSet, format);
@@ -322,7 +322,7 @@ public class QueryOperation {
    * @param output the output stream to write to
    */
   public static void writeResult(Model model, Lang format, OutputStream output) {
-    if(format == null) {
+    if (format == null) {
       format = Lang.TTL;
     }
     RDFDataMgr.write(output, model, format);
