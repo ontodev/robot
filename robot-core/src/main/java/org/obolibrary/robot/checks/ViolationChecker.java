4a784a091bd9645f67e1a09f1a33b5a7209a02f4
Rebecca Tauber <becky.tauber@ninepts.com>
2018-01-26 11:46:25 -0500

    Revert "Change to Blazegraph for report"
    This reverts commit 320851a3474f8f0274b1599c4e070bc57345842a.

--- robot-core/src/main/java/org/obolibrary/robot/checks/ViolationChecker.java
+++ robot-core/src/main/java/org/obolibrary/robot/checks/ViolationChecker.java
@@ -1,30 +1,17 @@
 package org.obolibrary.robot.checks;
 
-import com.bigdata.journal.Options;
-import com.bigdata.rdf.sail.BigdataSail;
-import com.bigdata.rdf.sail.BigdataSailRepository;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.sparql.core.DatasetGraph;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
+import org.apache.commons.lang3.tuple.Triple;
+import org.obolibrary.robot.QueryOperation;
 import org.obolibrary.robot.UnmergeOperation;
-import org.openrdf.OpenRDFException;
-import org.openrdf.query.BindingSet;
-import org.openrdf.query.QueryLanguage;
-import org.openrdf.query.TupleQuery;
-import org.openrdf.query.TupleQueryResult;
-import org.openrdf.repository.RepositoryConnection;
-import org.openrdf.rio.RDFFormat;
-import org.semanticweb.owlapi.formats.TurtleDocumentFormat;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,54 +21,22 @@ public class ViolationChecker {
   private static final Logger logger = LoggerFactory.getLogger(UnmergeOperation.class);
 
   /**
-   * Display number of violations based on severity level to the user as logger errors.
-   * If no violations are found, no errors are returned. TODO: Formated reports
-   * 
-   * @param ontology OWLOntology to report
-   * @param queries Set of CheckerQueries
-   * @throws Exception on any issue
+   * Display number of violations by severity as ERROR. If no violations, just provide INFO. TODO:
+   * Detailed logging on triples from each query.
+   *
+   * @param ontology OWLOntology to report on
+   * @param queries Set of CheckerQuery objects
+   * @throws Exception on any problem
    */
   public static void getViolations(OWLOntology ontology, Set<CheckerQuery> queries)
       throws Exception {
-    File jnl = File.createTempFile("report", ".jnl");
-    Properties props = new Properties();
-    props.put(Options.BUFFER_MODE, "DiskRW");
-    props.put(Options.FILE, jnl.getAbsolutePath());
-
-    BigdataSailRepository repo = new BigdataSailRepository(new BigdataSail(props));
-    repo.initialize();
-
-    RepositoryConnection conn = repo.getConnection();
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    OWLOntologyManager manager = ontology.getOWLOntologyManager();
-
-    // Load the ontology to the repo
-    try {
-      // Save in turtle format
-      manager.saveOntology(ontology, new TurtleDocumentFormat(), out);
-      conn.begin();
-      try {
-        // Read into the repo
-        try (Reader r = new InputStreamReader(new ByteArrayInputStream(out.toByteArray()))) {
-          conn.add(r, manager.getOntologyDocumentIRI(ontology).toString(), RDFFormat.TURTLE);
-        }
-        conn.commit();
-      } catch (OpenRDFException e) {
-        conn.rollback();
-        throw e;
-      }
-    } finally {
-      conn.close();
-      out.close();
-    }
-
-    // Store severity level violations separately
-    Map<String, List<BindingSet>> severity1 = new HashMap<>();
-    Map<String, List<BindingSet>> severity2 = new HashMap<>();
-    Map<String, List<BindingSet>> severity3 = new HashMap<>();
-    Map<String, List<BindingSet>> severity4 = new HashMap<>();
-    Map<String, List<BindingSet>> severity5 = new HashMap<>();
-
+	// Store severity level violations separately
+    Map<String, List<Triple<String, String, String>>> severity1 = new HashMap<>();
+    Map<String, List<Triple<String, String, String>>> severity2 = new HashMap<>();
+    Map<String, List<Triple<String, String, String>>> severity3 = new HashMap<>();
+    Map<String, List<Triple<String, String, String>>> severity4 = new HashMap<>();
+    Map<String, List<Triple<String, String, String>>> severity5 = new HashMap<>();
+    
     // Track number of violations for each severity level
     Integer count1 = 0;
     Integer count2 = 0;
@@ -89,35 +44,31 @@ public class ViolationChecker {
     Integer count4 = 0;
     Integer count5 = 0;
 
-    conn = repo.getReadOnlyConnection();
-    try {
-	    for (CheckerQuery query : queries) {
-	      List<BindingSet> violations;
-	      switch (query.severity) {
-	        case 1:
-	          violations = getViolations(conn, query);
-	          severity1.put(query.title, violations);
-	          count1 += violations.size();
-	        case 2:
-	          violations = getViolations(conn, query);
-	          severity2.put(query.title, violations);
-	          count2 += violations.size();
-	        case 3:
-	          violations = getViolations(conn, query);
-	          severity3.put(query.title, violations);
-	          count3 += violations.size();
-	        case 4:
-	          violations = getViolations(conn, query);
-	          severity4.put(query.title, violations);
-	          count4 += violations.size();
-	        case 5:
-	          violations = getViolations(conn, query);
-	          severity5.put(query.title, violations);
-	          count5 += violations.size();
-	      }
-	    }
-    } finally {
-    	conn.close();
+    DatasetGraph dsg = QueryOperation.loadOntology(ontology);
+    for (CheckerQuery query : queries) {
+      List<Triple<String, String, String>> violations;
+      switch (query.severity) {
+        case 1:
+          violations = getViolations(dsg, query);
+          severity1.put(query.title, violations);
+          count1 += violations.size();
+        case 2:
+          violations = getViolations(dsg, query);
+          severity2.put(query.title, violations);
+          count2 += violations.size();
+        case 3:
+          violations = getViolations(dsg, query);
+          severity3.put(query.title, violations);
+          count3 += violations.size();
+        case 4:
+          violations = getViolations(dsg, query);
+          severity4.put(query.title, violations);
+          count4 += violations.size();
+        case 5:
+          violations = getViolations(dsg, query);
+          severity5.put(query.title, violations);
+          count5 += violations.size();
+      }
     }
 
     Integer violations = count1 + count2 + count3 + count4 + count5;
@@ -133,23 +84,40 @@ public class ViolationChecker {
     }
   }
 
-
   /**
-   * Returns a list of BindingSets from violation query results.
-   * @param  conn open RepositoryConnection
-   * @param  query CheckerQuery
-   * @return List of BindingSets
-   * @throws Exception on any issue
+   * Get the violations from one CheckerQuery.
+   *
+   * @param ontology OWLOntology to report on
+   * @param query CheckerQuery object with title and queryString
+   * @return Map with query title as key and list of returned triples as value
+   * @throws Exception on any problem
    */
-  private static List<BindingSet> getViolations(RepositoryConnection conn, CheckerQuery query)
-      throws Exception {
-    TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, query.queryString);
-    // query.setIncludeInferred(true);
-    TupleQueryResult result = tupleQuery.evaluate();
+  private static List<Triple<String, String, String>> getViolations(DatasetGraph dsg,
+	  CheckerQuery query) throws Exception {
+    ResultSet violationSet = QueryOperation.execQuery(dsg, query.queryString);
 
-    List<BindingSet> violations = new ArrayList<>();
-    while (result.hasNext()) {
-      violations.add(result.next());
+    List<Triple<String, String, String>> violations = new ArrayList<>();
+    while (violationSet.hasNext()) {
+      QuerySolution violation = violationSet.next();
+      String entity;
+      String property;
+      String value;
+      try {
+        entity = violation.get("entity").toString();
+      } catch (NullPointerException e) {
+        entity = null;
+      }
+      try {
+        property = violation.get("property").toString();
+      } catch (NullPointerException e) {
+        property = null;
+      }
+      try {
+        value = violation.get("value").toString();
+      } catch (NullPointerException e) {
+        value = null;
+      }
+      violations.add(Triple.of(entity, property, value));
     }
     return violations;
   }
