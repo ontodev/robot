ed12a54c1503ff0dabda7efc421c52a1045f4677
Michael Cuffaro <mike@gwydion>
2019-08-16 11:15:47 -0400

    make row index and column index into static variables of the ValidateOperation class, and change naming convention: all static members of ValidateOperation (methods and variables) are named using underscore convention, while local method-internal variables are named using camelCase

--- robot-core/src/main/java/org/obolibrary/robot/ValidateOperation.java
+++ robot-core/src/main/java/org/obolibrary/robot/ValidateOperation.java
@@ -17,8 +17,6 @@ import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
-import org.semanticweb.owlapi.util.ShortFormProvider;
-import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +37,9 @@ import org.slf4j.LoggerFactory;
  * @author <a href="mailto:consulting@michaelcuffaro.com">Michael E. Cuffaro</a>
  */
 public class ValidateOperation {
+  // Naming convention: methods and static variables are named using the underscore convention,
+  // local (method-internal) variables are named using camelCase
+
   /** Logger */
   private static final Logger logger = LoggerFactory.getLogger(ValidateOperation.class);
 
@@ -46,16 +47,22 @@ public class ValidateOperation {
   private static Writer writer;
 
   /** The reasoner factory to use for validation */
-  private static OWLReasonerFactory reasonerFactory;
+  private static OWLReasonerFactory reasoner_factory;
 
   /** The ontology to use for validation */
   private static OWLOntology ontology;
 
   /** A map from rdfs:labels to IRIs */
-  private static Map<String, IRI> labelToIriMap;
+  private static Map<String, IRI> label_to_iri_map;
 
   /** A map from IRIs to rdfs:labels */
-  private static Map<IRI, String> iriToLabelMap;
+  private static Map<IRI, String> iri_to_label_map;
+
+  /** The row of CSV data currently being processed */
+  private static int csv_row_index;
+
+  /** The column of CSV data currently being processed */
+  private static int csv_col_index;
 
   /**
    * INSERT DOC HERE
@@ -72,7 +79,7 @@ public class ValidateOperation {
     initialize(ontology, reasonerFactory, writer);
 
     // Create a new reasoner, from the reasoner factory, based on the ontology data:
-    OWLReasoner reasoner = ValidateOperation.reasonerFactory.createReasoner(ValidateOperation.ontology);
+    OWLReasoner reasoner = reasoner_factory.createReasoner(ontology);
 
     // Extract the header and rules rows from the CSV data and map the column names to their
     // associated rules:
@@ -80,28 +87,28 @@ public class ValidateOperation {
     List<String> allRules = csvData.remove(0);
     HashMap<String, Map<String, String>> headerToRuleMap = new HashMap();
     for (int i = 0; i < header.size(); i++) {
-      headerToRuleMap.put(header.get(i), parse_rules(i, allRules.get(i)));
+      headerToRuleMap.put(header.get(i), parse_rules(allRules.get(i)));
     }
 
     // Validate the data rows:
-    for (int rowIndex = 0; rowIndex < csvData.size(); rowIndex++) {
-      List<String> row = csvData.get(rowIndex);
-      for (int colIndex = 0; colIndex < header.size(); colIndex++) {
-        String colName = header.get(colIndex);
+    for (csv_row_index = 0; csv_row_index < csvData.size(); csv_row_index++) {
+      List<String> row = csvData.get(csv_row_index);
+      for (csv_col_index = 0; csv_col_index < header.size(); csv_col_index++) {
+        String colName = header.get(csv_col_index);
         Map<String, String> colRules = headerToRuleMap.get(colName);
 
         // Get the contents of the current cell (the 'child data')
-        String childCell = row.get(colIndex).trim();
+        String childCell = row.get(csv_col_index).trim();
         if (childCell.equals("")) continue;
 
         // Get the rdfs:label and IRI corresponding to the child:
         String childLabel = get_label_from_term(childCell);
         if (childLabel == null) {
           writeout(
-              "Could not find '" + childCell + "' in ontology", rowIndex, colIndex);
+              "Could not find '" + childCell + "' in ontology");
           continue;
         }
-        IRI child = ValidateOperation.labelToIriMap.get(childLabel);
+        IRI child = label_to_iri_map.get(childLabel);
         logger.debug("Found child: " + child.toString() + " with label: " + childLabel);
 
         // Below, perform further validation depending on any rules that have been defined for this
@@ -109,21 +116,18 @@ public class ValidateOperation {
 
         // The 'the entity in this column has the following axiom' rule:
         if (colRules.containsKey("has-axiom")) {
-          validate_axioms(
-              child, childLabel, colRules.get("has-axiom"), reasoner, row, rowIndex, colIndex);
+          validate_axioms(child, childLabel, colRules.get("has-axiom"), reasoner, row);
         }
 
         // The 'the entity in this column has the following subclasses' rule:
         // NOTE: Possibly this validator can be subsumed under validate_axioms
         if (colRules.containsKey("sc")) {
-          validate_ancestry(
-              child, childLabel, colRules.get("sc"), reasoner, row, rowIndex, colIndex);
+          validate_ancestry(child, childLabel, colRules.get("sc"), reasoner, row);
         }
 
         // The 'the entity in this column is the same as the one in that column' rule:
         if (colRules.containsKey("same-as")) {
-          validate_twins(
-              child, childLabel, colRules.get("same-as"), reasoner, row, rowIndex, colIndex);
+          validate_twins(child, childLabel, colRules.get("same-as"), reasoner, row);
         }
       }
     }
@@ -139,19 +143,20 @@ public class ValidateOperation {
       Writer writer) {
 
     ValidateOperation.ontology = ontology;
-    ValidateOperation.reasonerFactory = reasonerFactory;
+    ValidateOperation.reasoner_factory = reasonerFactory;
     ValidateOperation.writer = writer;
 
     // Extract from the ontology two maps from rdfs:labels to IRIs and vice versa:
-    ValidateOperation.iriToLabelMap = OntologyHelper.getIRILabels(ValidateOperation.ontology);
-    ValidateOperation.labelToIriMap = reverse_iri_label_map(ValidateOperation.iriToLabelMap);
+    ValidateOperation.iri_to_label_map = OntologyHelper.getIRILabels(ValidateOperation.ontology);
+    ValidateOperation.label_to_iri_map = reverse_iri_label_map(ValidateOperation.iri_to_label_map);
   }
 
   /**
    * INSERT DOC HERE
    */
-  private static void writeout(String msg, int rowIndex, int colIndex) throws IOException {
-    writer.write(String.format("At row: %d, column: %d: %s\n", rowIndex + 1, colIndex + 1, msg));
+  private static void writeout(String msg) throws IOException {
+    writer.write(String.format("At row: %d, column: %d: %s\n",
+                               csv_row_index + 1, csv_col_index + 1, msg));
   }
 
   /**
@@ -176,7 +181,7 @@ public class ValidateOperation {
   /**
    * INSERT DOC HERE
    */
-  private static Map<String, String> parse_rules(int colIndex, String ruleString) {
+  private static Map<String, String> parse_rules(String ruleString) {
     HashMap<String, String> ruleMap = new HashMap();
     String[] rules = ruleString.split("\\s*;\\s*");
     for (String rule : rules) {
@@ -184,7 +189,7 @@ public class ValidateOperation {
       String ruleKey = ruleParts[0].trim();
       String ruleVal = ruleParts[1].trim();
       if (ruleMap.containsKey(ruleKey)) {
-        logger.warn("Duplicate rule: '" + ruleKey + "' in column " + (colIndex + 1));
+        logger.warn("Duplicate rule: '" + ruleKey + "' in column " + (csv_col_index + 1));
       }
       ruleMap.put(ruleKey, ruleVal);
     }
@@ -196,15 +201,15 @@ public class ValidateOperation {
    */
   private static String get_label_from_term(String term) {
     // If the term is already a recognised label, then just send it back:
-    if (ValidateOperation.labelToIriMap.containsKey(term)) {
+    if (label_to_iri_map.containsKey(term)) {
       return term;
     }
 
     // Check to see if the term is a recognised IRI (possibly in short form), and if so return its
     // corresponding label:
-    for (IRI iri : ValidateOperation.iriToLabelMap.keySet()) {
+    for (IRI iri : iri_to_label_map.keySet()) {
       if (iri.toString().equals(term) || iri.getShortForm().equals(term)) {
-        return ValidateOperation.iriToLabelMap.get(iri);
+        return iri_to_label_map.get(iri);
       }
     }
 
@@ -243,9 +248,7 @@ public class ValidateOperation {
       String childLabel,
       String axiom,
       OWLReasoner reasoner,
-      List<String> row,
-      int rowIndex,
-      int colIndex) throws Exception, IOException {
+      List<String> row) throws Exception, IOException {
 
     // TODO: Add a debug statement like this one to every validate_ function.
     logger.debug(String.format(
@@ -255,19 +258,20 @@ public class ValidateOperation {
         "axiom: '%s', " +
         "reasoner: '%s', " +
         "row: '%s', " +
-        "rowIndex: '%s', " +
-        "colIndex: '%s'. ",
+        "csv_row_index: '%s', " +
+        "csv_col_index: '%s'. ",
         child.getShortForm(), childLabel, axiom, reasoner.getClass().getSimpleName(), row,
-        rowIndex, colIndex));
+        csv_row_index, csv_col_index));
 
-    OWLClass childClass = OntologyHelper.getEntity(ValidateOperation.ontology, child).asOWLClass();
+    OWLClass childClass = OntologyHelper.getEntity(ontology, child).asOWLClass();
+
     ManchesterSyntaxTool parser = new ManchesterSyntaxTool(ontology);
     OWLClassExpression ce;
     try {
       ce = parser.parseManchesterExpression(axiom);
     }
     catch (ParserException e) {
-      writeout("Unable to parse axiom: '" + axiom + "'", rowIndex, colIndex);
+      writeout("Unable to parse axiom: '" + axiom + "'");
       return;
     }
     logger.debug("Class expression: " + ce);
@@ -280,8 +284,7 @@ public class ValidateOperation {
       writeout(
           String.format(
               "%s (%s) is not a direct descendant of '%s'\n",
-              child.getShortForm(), childLabel, axiom),
-          rowIndex, colIndex);
+              child.getShortForm(), childLabel, axiom));
     }
 
     parser.dispose();
@@ -295,26 +298,23 @@ public class ValidateOperation {
       String childLabel,
       String parentRule,
       OWLReasoner reasoner,
-      List<String> row,
-      int rowIndex,
-      int colIndex)
+      List<String> row)
       throws Exception {
 
     String parentLabel = construct_label_from_rule(parentRule, row);
     if (parentLabel == null) {
-      writeout(
-          "Could not determine parent from rule '" + parentRule + "'", rowIndex, colIndex);
+      writeout("Could not determine parent from rule '" + parentRule + "'");
       return;
     }
 
-    IRI parent = ValidateOperation.labelToIriMap.get(parentLabel);
+    IRI parent = label_to_iri_map.get(parentLabel);
     logger.debug("Found parent: " + parent.toString() + " with label: " + parentLabel);
 
     // Get the OWLClass corresponding to the parent:
-    OWLClass parentClass = OntologyHelper.getEntity(ValidateOperation.ontology, parent).asOWLClass();
+    OWLClass parentClass = OntologyHelper.getEntity(ontology, parent).asOWLClass();
 
     // Get the OWLClass corresponding to the child, and its super classes:
-    OWLClass childClass = OntologyHelper.getEntity(ValidateOperation.ontology, child).asOWLClass();
+    OWLClass childClass = OntologyHelper.getEntity(ontology, child).asOWLClass();
     NodeSet<OWLClass> childAncestors = reasoner.getSuperClasses(childClass, false);
 
     // Check if the child's ancestors include the parent:
@@ -322,8 +322,7 @@ public class ValidateOperation {
       writeout(
           String.format(
               "%s (%s) is not a descendant of %s (%s)\n",
-              child.getShortForm(), childLabel, parent.getShortForm(), parentLabel),
-          rowIndex, colIndex);
+              child.getShortForm(), childLabel, parent.getShortForm(), parentLabel));
     }
     logger.info(
         String.format("Relationship between '%s' and '%s' is valid.", childLabel, parentLabel));
@@ -337,24 +336,20 @@ public class ValidateOperation {
       String jacobLabel,
       String esauRule,
       OWLReasoner reasoner,
-      List<String> row,
-      int rowIndex,
-      int colIndex) throws IOException {
+      List<String> row) throws IOException {
 
     String esauLabel = construct_label_from_rule(esauRule, row);
     if (esauLabel == null) {
-      writeout(
-          "Could not determine twin cell from rule '" + esauRule + "'", rowIndex, colIndex);
+      writeout("Could not determine twin cell from rule '" + esauRule + "'");
       return;
     }
 
-    IRI esau = ValidateOperation.labelToIriMap.get(esauLabel);
+    IRI esau = label_to_iri_map.get(esauLabel);
     if (!esau.equals(jacob)) {
       writeout(
           String.format(
               "Cell's IRI: %s (%s) does not match IRI: %s (%s) inferred from rule '%s'",
-              jacob.getShortForm(), jacobLabel, esau.getShortForm(), esauLabel, esauRule),
-          rowIndex, colIndex);
+              jacob.getShortForm(), jacobLabel, esau.getShortForm(), esauLabel, esauRule));
     }
 
     logger.info(
