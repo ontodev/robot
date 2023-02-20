5cab7aa1f456cd7473801c697d7ceebc4468f2a4
hkir-dev <huseyin_kir@yahoo.com>
2023-02-20 19:08:09 +0000

    rename logging parameter issue #886

--- robot-command/src/main/java/org/obolibrary/robot/RemoveCommand.java
+++ robot-command/src/main/java/org/obolibrary/robot/RemoveCommand.java
@@ -161,7 +161,7 @@ public class RemoveCommand implements Command {
         dropParameters.stream()
             .filter(s -> !s.equalsIgnoreCase("all"))
             .map(
-                curie -> CommandLineHelper.maybeCreateIRI(ioHelper, curie, "drop-axiom-annotation"))
+                curie -> CommandLineHelper.maybeCreateIRI(ioHelper, curie, "drop-axiom-annotations"))
             .collect(Collectors.toList());
 
     // Get the objects to remove
