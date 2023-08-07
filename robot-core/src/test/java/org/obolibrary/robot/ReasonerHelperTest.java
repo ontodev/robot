49a569e8fedecb04433e0369fd0bc65130214d0a
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2023-08-07 14:56:48 +0300

    Update ReasonerHelperTest.java

--- robot-core/src/test/java/org/obolibrary/robot/ReasonerHelperTest.java
+++ robot-core/src/test/java/org/obolibrary/robot/ReasonerHelperTest.java
@@ -49,8 +49,6 @@ public class ReasonerHelperTest extends CoreTest {
       } catch (IncoherentRBoxException e) {
         isCaughtException = true;
       }
-      // To our great disappointment, ELK does not recognise this incoherent R Box
-      // https://github.com/ontodev/robot/pull/999#issuecomment-1666567017
       assertTrue(isCaughtException);
     }
   }
